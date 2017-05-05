200b752cc4729c7c3c3e128303b22ca02c89dc01
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-05 12:48:40 -0700

    Added functionality to add and delete games

--- party.py
+++ party.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, session, render_template, request, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from model import Game, connect_to_db
+from model import Game, connect_to_db, db
 
 app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRET"
@@ -37,6 +37,31 @@ def games():
     else:
         flash("YOU MUST LOGIN TO SEE THE GAMES!!!!")
         return redirect("/")
+
+
+@app.route("/games", methods=["POST"])
+def add_game():
+    new_game = request.form.get("game_name")
+    new_game_description = request.form.get("game_description")
+
+    g = Game(name=new_game, description=new_game_description)
+    db.session.add(g)
+    db.session.commit()
+
+    flash("Game successfully added!")
+    return redirect("/games")
+
+
+@app.route("/game-delete", methods=["POST"])
+def delete_game():
+    game = request.form.get("game_name")
+
+    game_object = Game.query.filter(Game.name == game).one()
+    db.session.delete(game_object)
+    db.session.commit()
+
+    flash("Game successfully deleted :(")
+    return redirect("/games")
 
 
 if __name__ == "__main__":
